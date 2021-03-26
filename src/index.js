9d26e85bb020bd9aea11ae867e250614b01a6df7
Suman Sarkar <sumansarkarwd@gmail.com>
2021-03-27 02:35:01 +0530

    Write first query

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-import { message } from "./messages";
+import { GraphQLServer } from "graphql-yoga";
 
-console.log({ message });
+// Type definition (schema)
+const typeDefs = `
+    type Query {
+        hello: String
+        name: String
+        location: String! # '!' denotes that this property cannot return null
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return "Hello world!";
+        },
+        name() {
+            return "John";
+        },
+        location() {
+            return "Kolkata";
+        }
+    }
+}
+
+// configure server
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+// start server
+server.start(() => {
+    console.log("Server running!!!");
+})
