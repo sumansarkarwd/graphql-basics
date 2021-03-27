ebb0848bc19962c0c227ec70ec4aa9cc681d9175
Suman Sarkar <sumansarkarwd@gmail.com>
2021-03-27 18:38:09 +0530

    Custom types

--- src/index.js
+++ src/index.js
@@ -3,39 +3,43 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definition (schema)
 const typeDefs = `
     type Query {
+        me: User!
+        post: Post
+    }
+
+    type User {
         id: ID!
-        hello: String
-        name: String
-        age: Int!
-        admin: Boolean
-        score: Float
-        location: String! # '!' denotes that this property cannot return null
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean
     }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    id() {
-      return "abc_123";
+    me() {
+      return {
+        id: "abc_1",
+        name: "John",
+        email: "john@mail.com",
+        age: 18,
+      };
     },
-    age() {
-      return 18;
-    },
-    admin() {
-      return true;
-    },
-    score() {
-      return 65.7;
-    },
-    hello() {
-      return "Hello world!";
-    },
-    name() {
-      return "John";
-    },
-    location() {
-      return "Kolkata";
+    post() {
+      return {
+        id: "abc_2",
+        title: "Post 1",
+        body: "lorem ipsum",
+        published: true,
+      };
     },
   },
 };
