5f798f76dcff389396e8f2b3f55c54892e7c471e
Suman Sarkar <sumansarkarwd@gmail.com>
2021-03-27 19:31:52 +0530

    Working with custom array types

--- src/index.js
+++ src/index.js
@@ -1,11 +1,59 @@
 import { GraphQLServer } from "graphql-yoga";
 
+const users = [
+  {
+    id: "abc_1",
+    name: "John",
+    email: "john@mail.com",
+    age: 18,
+  },
+  {
+    id: "abc_2",
+    name: "Jane",
+    email: "jane@mail.com",
+    age: 19,
+  },
+  {
+    id: "abc_3",
+    name: "Adam",
+    email: "adam@mail.com",
+    age: 17,
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body:
+      "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    published: true,
+  },
+  {
+    id: 2,
+    title: "qui est esse",
+    body:
+      "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+    published: true,
+  },
+  {
+    id: 3,
+    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    body:
+      "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+    published: false,
+  },
+];
+
 // Type definition (schema)
 const typeDefs = `
     type Query {
         me: User!
         post: Post
         add(numbers: [Float!]!): Float
+        users(query: String): [User]!
+        posts(query: String): [Post]!
     }
 
     type User {
@@ -43,12 +91,28 @@ const resolvers = {
       };
     },
     add(parent, args, ctx, info) {
-        if(!args.numbers.length) return 0;
+      if (!args.numbers.length) return 0;
 
-        return args.numbers.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue;
-        })
-    }
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      });
+    },
+    users(parent, args, ctx, info) {
+      if (!args.query || !args.query === "") return users;
+
+      const regexObj = new RegExp(args.query, "i");
+
+      return users.filter((user) => regexObj.test(user.name));
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query || !args.query === "") return posts;
+
+      const regexObj = new RegExp(args.query, "i");
+
+      return posts.filter(
+        (user) => regexObj.test(user.title) || regexObj.test(user.body)
+      );
+    },
   },
 };
 
