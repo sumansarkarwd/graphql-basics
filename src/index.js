462e457d6e8e456a6d8936a070b804655be5c130
Suman Sarkar <sumansarkarwd@gmail.com>
2021-03-27 18:25:49 +0530

    Scaler types

--- src/index.js
+++ src/index.js
@@ -3,34 +3,50 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definition (schema)
 const typeDefs = `
     type Query {
+        id: ID!
         hello: String
         name: String
+        age: Int!
+        admin: Boolean
+        score: Float
         location: String! # '!' denotes that this property cannot return null
     }
-`
+`;
 
 // Resolvers
 const resolvers = {
-    Query: {
-        hello() {
-            return "Hello world!";
-        },
-        name() {
-            return "John";
-        },
-        location() {
-            return "Kolkata";
-        }
-    }
-}
+  Query: {
+    id() {
+      return "abc_123";
+    },
+    age() {
+      return 18;
+    },
+    admin() {
+      return true;
+    },
+    score() {
+      return 65.7;
+    },
+    hello() {
+      return "Hello world!";
+    },
+    name() {
+      return "John";
+    },
+    location() {
+      return "Kolkata";
+    },
+  },
+};
 
 // configure server
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
 // start server
 server.start(() => {
-    console.log("Server running!!!");
-})
+  console.log("Server running!!!");
+});
